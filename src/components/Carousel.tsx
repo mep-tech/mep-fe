e3f5bfe83e18ce1512e8b35001cf7288812bf09d
Tsa2341 <alanshema2002@gmail.com>
2024-09-07 23:07:41 +0200

    Fixed carousel bugs and connected projects cards

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -9,23 +9,27 @@ export type CarouselRef = {
   onPreviousClick: () => void;
   onNextClick: () => void;
 };
-
 type CarouselProps = {
   children: ReactElement<any, string | React.JSXElementConstructor<any>>[];
   className?: string;
+  slideDelay?: number;
+  slideDuration?: number;
+  wrap?: boolean;
   [props: string]: any;
 };
 
 const Carousel = forwardRef<CarouselRef, CarouselProps>(
-  ({ children, className, slideDelay = 3, slideDuration = 1, wrap = true, ...props }, ref) => {
-    const animateSlides = useRef<(direction: number) => void>(() => {});
+  ({ children, className, slideDelay = 3, slideDuration = 2, wrap = true, ...props }, ref) => {
+    const animateSlides = useRef<(direction: number, onComplete?: () => void) => void>(() => {});
+    const slideContainerRef = useRef<HTMLDivElement>(null);
 
     useLayoutEffect(() => {
       const eventDisposable: any[] = [];
+      const slides = slideContainerRef.current?.children as HTMLCollectionOf<HTMLElement>;
+
+      if (!slides) return;
 
       const ctx = gsap.context(() => {
-        const slidesContainerId = "#carousel-container";
-        const slideClass = ".carousel-slide";
         const numSlides = children.length;
         const progressWrap = gsap.utils.wrap(0, 1);
         const wrapX = gsap.utils.wrap(-100, (numSlides - 1) * 100);
@@ -40,8 +44,8 @@ const Carousel = forwardRef<CarouselRef, CarouselProps>(
           const snapped = gsap.utils.snap(slideWidth, value);
           return wrap ? snapped : gsap.utils.clamp(-slideWidth * (numSlides - 1), 0, snapped);
         };
-        animateSlides.current = (direction: number) => {
-          timer.restart(true);
+        animateSlides.current = (direction: number, onComplete?: () => void) => {
+          timer.pause();
           slideAnimation.kill();
           const x = snapX(+gsap.getProperty(proxy, "x") + direction * slideWidth);
 
@@ -49,12 +53,16 @@ const Carousel = forwardRef<CarouselRef, CarouselProps>(
             x: x,
             duration: slideDuration,
             onUpdate: updateProgress,
+            onComplete: () => {
+              timer.restart(true);
+              if (onComplete) onComplete();
+            },
           });
         };
 
         const autoPlay = () => {
           if (draggable.isPressed || draggable.isDragging || draggable.isThrowing) {
-            timer.restart(true);
+            timer.pause();
           } else {
             animateSlides.current(-1);
           }
@@ -65,36 +73,32 @@ const Carousel = forwardRef<CarouselRef, CarouselProps>(
           animation.progress(progressWrap(+gsap.getProperty(proxy, "x") / wrapWidth));
         };
         function updateDraggable(this: any) {
-          timer.restart(true);
+          timer.pause();
           slideAnimation.kill();
           this.update();
         }
         const resize = () => {
           const norm = +gsap.getProperty(proxy, "x") / wrapWidth || 0;
 
-          slideWidth = document.querySelectorAll<HTMLElement>(slideClass)[0].offsetWidth;
+          slideWidth = slides[0].offsetWidth;
           wrapWidth = slideWidth * numSlides;
 
-          if (wrap) {
-            draggable.applyBounds({ minX: -slideWidth * (numSlides - 1), maxX: 0 });
-          }
+          if (!wrap) draggable.applyBounds({ minX: -slideWidth * (numSlides - 1), maxX: 0 });
 
-          gsap.set(proxy, {
-            x: norm * wrapWidth,
-          });
+          gsap.set(proxy, { x: norm * wrapWidth });
 
           animateSlides.current(0);
           slideAnimation.progress(1);
         };
 
         // Make slides position horizontally concurrently
-        gsap.set(slideClass, {
+        gsap.set(slides, {
           xPercent: (i) => i * 100,
         });
 
         // Creates an animation that moves slides horizontally by their total width
         // and repeats infinitely and wraps position
-        const animation = gsap.to(slideClass, {
+        const animation = gsap.to(slides, {
           xPercent: "+=" + numSlides * 100,
           duration: 1,
           ease: "none",
@@ -106,10 +110,19 @@ const Carousel = forwardRef<CarouselRef, CarouselProps>(
         });
 
         const draggable = new Draggable(proxy, {
-          trigger: slidesContainerId,
+          trigger: slideContainerRef.current,
           onPress: updateDraggable,
           onDrag: updateProgress,
           onThrowUpdate: updateProgress,
+          onDragEnd: () => {
+            const prevDuration = slideDuration;
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+            slideDuration = 1;
+            animateSlides.current(0, () => {
+              console.log(slideDuration, prevDuration);
+              slideDuration = prevDuration;
+            });
+          },
           snap: {
             x: snapX,
           },
@@ -129,21 +142,23 @@ const Carousel = forwardRef<CarouselRef, CarouselProps>(
     }, [children, slideDelay, slideDuration, wrap]);
 
     useImperativeHandle(ref, () => ({
-      onPreviousClick: () => animateSlides.current(-1),
-      onNextClick: () => animateSlides.current(1),
+      onPreviousClick: () => {
+        animateSlides.current(1);
+      },
+      onNextClick: () => {
+        animateSlides.current(-1);
+      },
     }));
 
     return (
-      <div id="carousel-container" className={clsx("size-full overflow-hidden flex-1", className)} {...props}>
-        <div id="carousel-inner" className={"size-full overflow-hidden relative"}>
-          {children.map((slides, index) => {
-            return (
-              <div key={index} className="size-full absolute top-0 left-0 carousel-slide">
-                {slides}
-              </div>
-            );
-          })}
-        </div>
+      <div ref={slideContainerRef} className={clsx("size-full overflow-hidden flex-1 relative", className)} {...props}>
+        {children.map((slides, index) => {
+          return (
+            <div key={index} className="size-full absolute top-0 left-0">
+              {slides}
+            </div>
+          );
+        })}
       </div>
     );
   }
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,43 +1,50 @@
 import { Typography } from "@mui/material";
+import { forwardRef } from "react";
 
-const Title = ({
-  title,
-  color,
-  ...props
-}: {
-  title: string;
-  color?: "common.white" | "common.black" | "primary.main" | "secondary.main";
-  [props: string]: any;
-}) => {
-  return (
-    <Typography
-      sx={{
-        position: "relative",
-        fontWeight: 700,
-        fontSize: { xs: "32px", md: "40px" },
-        lineHeight: "1.2",
-        color: color,
-        zIndex: 1,
-        width: "fit-content",
-        pointerEvents: "none",
-        userSelect: "none",
-        "&::after": {
-          content: "' '",
-          position: "absolute",
-          width: "88%",
-          height: { xs: "10px", md: "12px" },
-          bottom: "5px",
-          left: "50%",
-          transform: "translateX(-50%)",
-          backgroundColor: "secondary.main",
-          zIndex: -1,
-        },
-      }}
-      {...props}
-    >
-      {title}
-    </Typography>
-  );
-};
+const Title = forwardRef(
+  (
+    {
+      title,
+      color,
+      ...props
+    }: {
+      title: string;
+      color?: "common.white" | "common.black" | "primary.main" | "secondary.main";
+      [props: string]: any;
+    },
+    ref: any
+  ) => {
+    return (
+      <Typography
+        ref={ref}
+        sx={{
+          position: "relative",
+          fontWeight: 700,
+          fontSize: { xs: "32px", md: "40px" },
+          lineHeight: "1.2",
+          color: color,
+          zIndex: 1,
+          width: "fit-content",
+          pointerEvents: "none",
+          userSelect: "none",
+          "&::after": {
+            content: "' '",
+            position: "absolute",
+            width: "88%",
+            height: { xs: "10px", md: "12px" },
+            bottom: "5px",
+            left: "50%",
+            transform: "translateX(-50%)",
+            backgroundColor: "secondary.main",
+            zIndex: -1,
+          },
+        }}
+        {...props}
+      >
+        {title}
+      </Typography>
+    );
+  }
+);
 
 export default Title;
